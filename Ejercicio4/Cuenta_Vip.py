8d569e40b89b9cd2b36329ad8d41ac7fed5b0fa3
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-11-14 22:57:01 +0100

    cuenta vip

---
+++ Ejercicio4/Cuenta_Vip.py
@@ -0,0 +1,13 @@
+from CuentaBancaria import Cuenta_Bancaria
+
+
+class Cuenta_Vip(Cuenta_Bancaria):
+    def pedirsaldomin():
+        saldo_min = int(input('Escribe el saldo minimo: '))
+        return saldo_min
+
+    def comprobacion_saldo():
+        supera_minimo = False
+        saldo_min = pedirsaldomin()
+        if (Cuenta_Bancaria.getsaldo<saldo_min):
+            return print('No se puede tenemos menos dinero que '+ saldo_min)
